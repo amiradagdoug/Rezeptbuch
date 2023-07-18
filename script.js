2ebe3b6a767dd9613e29f5c841d58cb3ba7ec48e
amira <amira.dagdoug@docc.techstarter.de>
2023-07-18 23:13:51 +0200

    rezeptbuch

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".speiseplanFilter span").on("click", function () {
+    var currentClick = this.getAttribute("data-categ");
+    console.log("Current Click: " + currentClick);
+    $(".cardsList .card").each(function (index) {
+        if (currentClick == "*") {
+            $(this).show();
+        } else {
+            if (currentClick == this.getAttribute("data-categ")) {
+                $(this).slideDown();
+            } else {
+                $(this).slideUp();
+            }
+        }
+    });
+});
+
+$(".card-action-plus").on("click", function () {
+    $(this).parent().parent().parent().find('.card-body-description').slideDown();
+});
